f2f8dfde756092380b7c0b73c7fd6e69c3c1767c
jonslucas <jon.s.lucas@gmail.com>
2015-07-06 20:55:16 -0400

    initial commit

---
+++ server/config/environment/production.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Production specific configuration
+// =================================
+module.exports = {
+  // Server IP
+  ip:       process.env.OPENSHIFT_NODEJS_IP ||
+            process.env.IP ||
+            undefined,
+
+  // Server port
+  port:     process.env.OPENSHIFT_NODEJS_PORT ||
+            process.env.PORT ||
+            8080,
+
+  // MongoDB connection options
+  mongo: {
+    uri:    process.env.'mongodb://heroku_vptx896k:usmbspkv45lh4ihi9f296orijo@ds051740.mongolab.com:51740/heroku_vptx896k' ||
+            process.env.MONGOHQ_URL ||
+            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+            'mongodb://localhost/basejumps'
+  }
+};
