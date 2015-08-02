3025bb7631a1b772ad03cb4dcb521854ad426cb7
jonslucas <jon.s.lucas@gmail.com>
2015-08-01 23:20:17 -0400

    Working with disqus integration.

--- client/app/comments/comments.directive.js
+++ client/app/comments/comments.directive.js
@@ -3,6 +3,7 @@
 var commCtrl = function($scope, Polls) {
   var ctrl = this;
   ctrl.showCreateComm = false;
+  ctrl.disqusUrl = 'https://mysterious-atoll-1051.herokuapp.com/'+$scope.poll.poll.author.name+'/'+$scope.poll.poll.question;
   ctrl.getComments = function (poll) {
     Polls.getComms(poll.poll._id, function(err, resp){
       if(err) { return console.error(err); }
